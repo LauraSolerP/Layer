3f226178cf17f791d78aa8ac617c24ffefd8ac4c
Laura Soler Pérez <lsoler17@gmail.com>
2025-11-21 16:59:44 +0100

    cosa

--- src/routes/Menu/patchMenuPrice.ts
+++ src/routes/Menu/patchMenuPrice.ts
@@ -3,11 +3,12 @@ import { UserMiddleware } from "../../middlewares/userMiddleware";
 import { PatchMenuPriceController } from "../../controllers/menuControllers/patchMenuPriceController";
 import { menuHelper } from "../../helpers/menuHelper";
 import { menuService } from "../../service/menuService";
+import { requireRole } from "../../middlewares/requireRoleMiddleware";
 
 const helper = new menuHelper()
 const service = new menuService(helper)
 const patchMenuPriceController = new PatchMenuPriceController(service)
 
 export function patchMenuPriceRoute(router: Router) {
-    router.patch("/v1/menu/:id", new UserMiddleware().run, patchMenuPriceController.run.bind(patchMenuPriceController))
+    router.patch("/v1/menu/:id", new UserMiddleware().run, requireRole(["OWNER"]), patchMenuPriceController.run.bind(patchMenuPriceController))
 }
